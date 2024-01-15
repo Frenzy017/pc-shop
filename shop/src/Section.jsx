c2a96e83ca0b32eabe382fd019ed8692d8ffd85d
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-01-15 20:07:08 +0200

    Create Section.jsx file

---
+++ shop/src/Section.jsx
@@ -0,0 +1,71 @@
+function Section() {
+    return (
+        <>
+            <section className="section">
+                <div className="flex-center">
+                    <h2 className="heading-primary">Recommended Configurations</h2>
+                </div>
+
+                <div className="grid-container u-margin-top-medium">
+
+                    <div className="grid-container__card">
+                        <img className="grid-container__pc" src="/img/White PC.webp" alt="white"/>
+                        <div className="grid-container__system u-margin-top-medium">
+                            <h4>Ultra 4090 Gaming PC</h4>
+                            <span>GPU: Nvidia RTX 4090 24GB</span>
+                            <span>CPU: AMD Ryzen 7700</span>
+                            <span>SSD: Kingston NVME 2TB PCIE 4.0</span>
+                            <span>RAM: Corsair 32GB DDR5 6000MHZ</span>
+                            <span>Motherboard: AMD B650 WiFi</span>
+                            <span>PCU: Seasonic FX 750W Gold rated</span>
+                        </div>
+                    </div>
+
+                    <div className="grid-container__card">
+                        <img className="grid-container__pc" src="/img/White PC.webp" alt="white"/>
+                        <div className="grid-container__system u-margin-top-medium">
+                            <h4>Ultra 4090 Gaming PC</h4>
+                            <span>GPU: Nvidia RTX 4090 24GB</span>
+
+                            <span>CPU: AMD Ryzen 7700</span>
+                            <span>SSD: Kingston NVME 2TB PCIE 4.0</span>
+                            <span>RAM: Corsair 32GB DDR5 6000MHZ</span>
+                            <span>Motherboard: AMD B650 WiFi</span>
+                            <span>PCU: Seasonic FX 750W Gold rated</span>
+                        </div>
+                    </div>
+
+                    <div className="grid-container__card">
+                        <img className="grid-container__pc" src="/img/White PC.webp" alt="white"/>
+                        <div className="grid-container__system u-margin-top-medium">
+                            <h4>Ultra 4090 Gaming PC</h4>
+                            <span>GPU: Nvidia RTX 4090 24GB</span>
+                            <span>CPU: AMD Ryzen 7700</span>
+                            <span>SSD: Kingston NVME 2TB PCIE 4.0</span>
+                            <span>RAM: Corsair 32GB DDR5 6000MHZ</span>
+                            <span>Motherboard: AMD B650 WiFi</span>
+                            <span>PCU: Seasonic FX 750W Gold rated</span>
+                        </div>
+                    </div>
+
+                    <div className="grid-container__card">
+                        <img className="grid-container__pc" src="/img/White PC.webp" alt="white"/>
+                        <div className="grid-container__system u-margin-top-medium">
+                            <h4>Ultra 4090 Gaming PC</h4>
+                            <span>GPU: Nvidia RTX 4090 24GB</span>
+                            <span>CPU: AMD Ryzen 7700</span>
+                            <span>SSD: Kingston NVME 2TB PCIE 4.0</span>
+                            <span>RAM: Corsair 32GB DDR5 6000MHZ</span>
+                            <span>Motherboard: AMD B650 WiFi</span>
+                            <span>PCU: Seasonic FX 750W Gold rated</span>
+                        </div>
+                    </div>
+
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Section
