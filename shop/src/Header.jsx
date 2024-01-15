536029383f2ba177acbfcd400f49c6c918ee4a8c
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-01-15 20:06:59 +0200

    Create Header.jsx file

---
+++ shop/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <header className="header">PC Store
+                <div className="flex-end flex-gap-15 ">
+                    <span className="header__total u-margin-right-large" >Total</span>
+                    <span className="header__buy u-margin-right-small">Buy</span>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
