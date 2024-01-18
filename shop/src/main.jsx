a4f32d71f6d134971a008ae575b11a4e4091a0cb
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-01-18 18:55:42 +0200

    Update file

--- shop/src/main.jsx
+++ shop/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import Header from "./Header.jsx";
 import Section from "./Section.jsx"
 
 
 import "/css/fonts.css"
+
+
 
 
 import "/img/White PC.webp"
@@ -16,17 +17,16 @@ import "/img/background-1.jpg"
 
 
 
-
-
-
 import "/sass/abstracts/_variables.scss"
 
 import "/sass/base/_base.scss"
 import "/sass/base/_typography.scss"
 import "/sass/base/_utilities.scss"
 
+import "/sass/components/_button.scss"
+
 import "/sass/layout/_header.scss"
-import "/sass/layout/_grid.scss"
+import "/sass/layout/_section.scss"
 import "/sass/layout/_navigation.scss"
 import "/sass/layout/_footer.scss"
 
@@ -36,6 +36,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Header/>
         <Section/>
-        <App/>
     </React.StrictMode>,
 )
