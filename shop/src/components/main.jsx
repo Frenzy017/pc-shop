fa3e398f69e1dacc0529d23e13326e584f5526c2
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-01-19 21:41:03 +0200

    Update import paths

--- shop/src/components/main.jsx
+++ shop/src/components/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from "./Header.jsx";
-import Section from "./Section.jsx"
+import Header from "/src/components/Header.jsx"
+import Section from "/src/components/Section.jsx"
 
 
 import "/css/fonts.css"
-
-
 
 
 import "/img/White PC.webp"
@@ -14,7 +12,6 @@ import "/img/Black PC.webp"
 import "/img/Black White PC.webp"
 import "/img/Purple PC.webp"
 import "/img/background-1.jpg"
-
 
 
 import "/sass/abstracts/_variables.scss"
@@ -29,7 +26,6 @@ import "/sass/layout/_header.scss"
 import "/sass/layout/_section.scss"
 import "/sass/layout/_navigation.scss"
 import "/sass/layout/_footer.scss"
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
