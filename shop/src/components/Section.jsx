780cf24a4e74e4ee04bdf7065720f3e06a98bbbe
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-01-19 21:38:15 +0200

    Update file

---
+++ shop/src/components/Section.jsx
@@ -0,0 +1,168 @@
+function Section() {
+    return (
+        <>
+            <section className="section">
+                <div className="flex-center">
+                    <h2 className="heading-primary">Explore our setups</h2>
+                </div>
+                <div className="section-container u-margin-top-medium">
+
+                    <div className="section-container__card">
+                        <img className="section-container__pc" src="/img/White PC.webp" alt="white"/>
+                        <div className="section-container__content">
+                            <p>Only</p>
+                            <p>3 left!</p>
+                        </div>
+                        <div className="section-container__system u-margin-top-medium">
+                            <h4>Ultra 4090 Gaming PC</h4>
+                            <div>
+                                <span className="bolder">GPU:</span>
+                                <span> GeForce RTX™ 4070 SUPER 12GB</span>
+                            </div>
+                            <div>
+                                <span className="bolder">CPU:</span>
+                                <span> AMD Ryzen™ 7 7800X3D Processor</span>
+                            </div>
+                            <div>
+                                <span className="bolder">SSD:</span>
+                                <span> 1TB WD Blue SN580 M.2 PCIe</span>
+                            </div>
+                            <div>
+                                <span className="bolder">RAM:</span>
+                                <span> Corsair 32GB DDR5 6000MHZ</span>
+                            </div>
+                            <div>
+                                <span className="bolder">Motherboard:</span>
+                                <span> MSI PRO B650-VC WIFI</span>
+                            </div>
+
+                            <div>
+                                <span className="bolder">PCU:</span>
+                                <span> Seasonic FX 750W Gold rated</span>
+                            </div>
+
+                            <div className="flex-center flex-start flex-gap-2 u-margin-top-medium ">
+                                <p className="section-container__price">Price: 1750$</p>
+                                <button className="btn u-margin-right-large">Buy</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="section-container__card">
+                        <img className="section-container__pc" src="/img/Black PC.webp" alt="white"/>
+                        <div className="section-container__system u-margin-top-medium">
+                            <h4>Ultra 4090 Gaming PC</h4>
+                            <div>
+                                <span className="bolder">GPU:</span>
+                                <span> GeForce RTX™ 4070 SUPER 12GB</span>
+                            </div>
+                            <div>
+                                <span className="bolder">CPU:</span>
+                                <span> AMD Ryzen™ 7 7800X3D Processor</span>
+                            </div>
+                            <div>
+                                <span className="bolder">SSD:</span>
+                                <span> 1TB WD Blue SN580 M.2 PCIe</span>
+                            </div>
+                            <div>
+                                <span className="bolder">RAM:</span>
+                                <span> Corsair 32GB DDR5 6000MHZ</span>
+                            </div>
+                            <div>
+                                <span className="bolder">Motherboard:</span>
+                                <span> MSI PRO B650-VC WIFI</span>
+                            </div>
+
+                            <div>
+                                <span className="bolder">PCU:</span>
+                                <span> Seasonic FX 750W Gold rated</span>
+                            </div>
+
+                            <div className="flex-center flex-start flex-gap-2 u-margin-top-medium ">
+                                <p className="section-container__price">Price: 1520$</p>
+                                <button className="btn u-margin-right-large">Buy</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="section-container__card">
+                        <img className="section-container__pc" src="/img/Black White PC.webp" alt="white"/>
+                        <div className="section-container__system u-margin-top-medium">
+                            <h4>Ultra 4070 Super Gaming PC</h4>
+                            <div>
+                                <span className="bolder">GPU:</span>
+                                <span> GeForce RTX™ 4070 SUPER 12GB</span>
+                            </div>
+                            <div>
+                                <span className="bolder">CPU:</span>
+                                <span> AMD Ryzen™ 7 7800X3D Processor</span>
+                            </div>
+                            <div>
+                                <span className="bolder">SSD:</span>
+                                <span> 1TB WD Blue SN580 M.2 PCIe</span>
+                            </div>
+                            <div>
+                                <span className="bolder">RAM:</span>
+                                <span> Corsair 32GB DDR5 6000MHZ</span>
+                            </div>
+                            <div>
+                                <span className="bolder">Motherboard:</span>
+                                <span> MSI PRO B650-VC WIFI</span>
+                            </div>
+
+                            <div>
+                                <span className="bolder">PCU:</span>
+                                <span> Seasonic FX 750W Gold rated</span>
+                            </div>
+
+                            <div className="flex-center flex-start flex-gap-2 u-margin-top-medium ">
+                                <p className="section-container__price">Price: 2750$</p>
+                                <button className="btn u-margin-right-large">Buy</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="section-container__card">
+                        <img className="section-container__pc" src="/img/Purple PC.webp" alt="white"/>
+                        <div className="section-container__system u-margin-top-medium">
+                            <h4>Creator PC Ultimate</h4>
+                            <div>
+                                <span className="bolder">GPU:</span>
+                                <span> GeForce RTX™ 4070 SUPER 12GB</span>
+                            </div>
+                            <div>
+                                <span className="bolder">CPU:</span>
+                                <span> AMD Ryzen™ 7 7800X3D Processor</span>
+                            </div>
+                            <div>
+                                <span className="bolder">SSD:</span>
+                                <span> 1TB WD Blue SN580 M.2 PCIe</span>
+                            </div>
+                            <div>
+                                <span className="bolder">RAM:</span>
+                                <span> Corsair 32GB DDR5 6000MHZ</span>
+                            </div>
+                            <div>
+                                <span className="bolder">Motherboard:</span>
+                                <span> MSI PRO B650-VC WIFI</span>
+                            </div>
+
+                            <div>
+                                <span className="bolder">PCU:</span>
+                                <span> Seasonic FX 750W Gold rated</span>
+                            </div>
+
+                            <div className="flex-center flex-start flex-gap-2 u-margin-top-medium ">
+                                <p className="section-container__price">Price: 3750$</p>
+                                <button className="btn u-margin-right-large">Buy</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Section
