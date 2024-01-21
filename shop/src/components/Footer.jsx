e3aa10a4d94df22227502822f964b55024b7ff33
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-01-21 23:46:24 +0200

    Create Footer.jsx file

---
+++ shop/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="footer__wrapper">
+                    <div className="footer__wrapper--icons">
+                        hello there
+                    </div>
+                </div>
+            </footer>
+
+        </>
+    )
+}
+
+export default Footer
